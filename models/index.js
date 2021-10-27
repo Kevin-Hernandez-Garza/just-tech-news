96aa5f3205b478178c5c92e921e2a8595932148a
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-27 03:10:47 -0500

    post routes added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
 // importing the User model, to collect User model data
 const User = require('./User');
 const Post = require('./Post');
+
+// creating associations 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+// relationship between the Post model to the User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
 // exporting the User object with it as a property
 module.exports = {User, Post};
