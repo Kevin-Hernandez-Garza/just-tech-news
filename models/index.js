6f1b9800a2513fc6aaacccd355cc7ed39d390ab5
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-27 01:05:26 -0500

    created Post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // importing the User model, to collect User model data
 const User = require('./User');
-
+const Post = require('./Post');
 // exporting the User object with it as a property
-module.exports = {User};
+module.exports = {User, Post};
