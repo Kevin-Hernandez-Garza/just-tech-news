305f6412608bb8e5268ca7d25c2e19f292a5e084
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-04 01:49:23 -0500

    conditionally rendered login/logout links

---
+++ public/javascript/upvote.js
@@ -0,0 +1,26 @@
+// define it async function because it will be making asynchronous PUT request with fetch() 
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
